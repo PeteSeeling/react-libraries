0b63a6bee60f8033a53d1144a5b88e674cd0968b
Pete Seeling <peteseeling@gmail.com>
2022-02-21 17:37:26 -0600

    data successfully console logged

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import DataGrid from 'react-data-grid';
+// import DataGrid from 'react-data-grid';
+import data from './data';
 import './App.css';
 
 function App() {
+  console.log(data);
   return (
     <div className="App">
      
